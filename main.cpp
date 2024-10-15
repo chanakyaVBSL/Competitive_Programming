293e63bd12e77eced5ad311d5dac19ea9c9a955c
vbslc <vbsl.chanakya6@gmail.com>
2024-10-15 10:46:03 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <print>
+
+using namespace std;
+
+int main() {
+    print("Hello, World!");
+    return 0;
+}
